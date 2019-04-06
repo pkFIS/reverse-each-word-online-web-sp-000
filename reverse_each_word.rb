216b9b7b7d71d08c4174d2eafeb826c72b0c2059
Paul K <pkfis@outlook.com>
2019-04-06 06:43:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,16 @@
 def reverse_each_word(sentence1)
   reversed_words = []
+  collected_words = []
   words = sentence1.split(" ")
     words.each do |word|
       reversed_words << word.reverse
     end
+    words.collect do |word|
+      reversed_words << word.reverse
+    end
   reversed_words.join(" ")
+    words.collect do |word|
+      collected_words << word.reverse
+  end
+  collected_words.join(" ")
 end
