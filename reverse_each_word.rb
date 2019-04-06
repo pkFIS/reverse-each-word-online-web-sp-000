75744882d10a0131a881e3da24eb4ce87fa95e44
Paul K <pkfis@outlook.com>
2019-04-06 06:31:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence1)
   words = sentence1.split(" ")
-    words.reverse#each do |word|
-  #  words = word.reverse
-  #end
-  #words.join(" ")
+    words.each do |word|
+      word.reverse
+    end
+  words
 end
